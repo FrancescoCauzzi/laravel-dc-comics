d0be911a5c296c2827ff0083f2ede295a7863dc5
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-16 18:16:53 +0200

    feat: style modifications

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,18 +3,31 @@
 
 @section('content')
 
-<main class="text-center">
-  <img src="{{$comic->thumb}}" alt="comic-image">
+<main class="__cshow-main">
+    <div class="container-fluid __thumb-ctn-fluid">
+        <div class="container __thumb-ctn">
 
-  <hr>
-
-  <h1>{{$comic->title}}</h1>
-
+            <img class="__thumb-img" src="{{$comic->thumb}}" alt="comic-image">
+        </div>
 
 
-  <p>
-    {{$comic->description}}
-  </p>
+
+
+    </div>
+    <div class="container py-5">
+
+
+
+        <hr>
+
+        <h1>{{$comic->title}}</h1>
+
+
+
+        <p>
+            {{$comic->description}}
+        </p>
+    </div>
 </main>
 
 @endsection
